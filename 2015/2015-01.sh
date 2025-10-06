f7f782a1e86036cf9b069ed62608d031ed172df9
maximizzar <mail@maximizzar.de>
2025-10-06 12:00:05 +0200

    add 2015 day 01

---
+++ 2015/2015-01.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: 2025 maximizzar <mail@maximizzar.de>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+is_basement() {
+	local open="$1"
+	local closed="$2"
+	if (( open - closed == -1 )); then
+		return 0
+	fi
+	return 1
+}
+
+main() {
+	local open
+	local closed
+	local lenght
+
+	while IFS= read -r -n1 parenthesis; do
+		if [[ "$parenthesis" == "(" ]]; then
+			(( open += 1 ))
+			(( lenght += 1 ))
+		elif [[ "$parenthesis" == ")" ]]; then
+			(( closed +=1 ))
+			(( lenght += 1 ))
+		fi
+
+		if is_basement "$open" "$closed"; then
+			printf "Basement is reached at: %s\n" "$lenght"
+		fi
+	done
+	printf "The final floor is: %s\n" "$(( open - closed ))"
+}
+
+main "$@"
