3b49b5e48af511e1e4e2279ee1712b337f22b55a
maximizzar <mail@maximizzar.de>
2026-07-08 17:33:18 +0200

    feat(wip): day 07 working parsing

--- 2015/2015-07.sh
+++ 2015/2015-07.sh
@@ -1,65 +1,60 @@
 #!/usr/bin/env bash
 
-# SPDX-FileCopyrightText: 2025 maximizzar <mail@maximizzar.de>
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
+declare -A wires=()
 
-# https://adventofcode.com/2015/day/7
+parse_line() {
+    expression_str="$1"
+    key="$2"
 
-# --- Day 7: Some Assembly Required ---
+    while IFS=' ' read -r a b c; do
+        # Handle NOT Operator
+        if [[ "$a" == "NOT" ]]; then
+            wires[$key]=$((!b))
+            return 0
+        fi
 
-# One func per gate
-# and or not functions with 2 or 1 input respertivly
-parse_signal() {
-	local raw_signal="$1"
+        # Handle Constances
+        if [[ "$a" =~ ^[0-9]+$ ]]; then
+            wires[$key]=$((a))
+            return 0
+        fi
 
-	local gate
-	local sig0
-	local sig1
-	local wire
+        # Handle Other Operators
+        # TODO: fix getting stored vars back for calc
+        case "$b" in
+        AND)
+            echo "${wires[$a]} AND ${wires[$c]} ->"
+            #wires[$key]=$((${!wires[$a]} & ${!wires[$c]}))
+            ;;
 
-	if [[ "$raw_signal" =~ ([A-Z]+) ]]; then
-		gate="${BASH_REMATCH[1]}"
-	fi
+        OR)
+            wires[$key]=$((a | c))
+            ;;
 
-	if [[ "$raw_signal" =~ ([0-1]+) ]] || [[ "$raw_signal" =~ ([a-z]+) ]]; then
-		sig0="${BASH_REMATCH[1]}"
-	fi
+        LSHIFT)
+            wires[$key]=$((a << c))
+            ;;
 
-	case "$gate" in
-		"AND")
-			echo "AND$raw_signal"
-			;;
-		"OR")
-			echo "OR $raw_signal"
-			;;
-		"LSHIFT")
-			echo "LSHIFT $raw_signal"
-			;;
-		"RSHIFT")
-			echo "RSHIFT $raw_signal"
-			;;
-		"NOT")
-			echo "NOT $raw_signal"
-			;;
-		*)
-			echo "$raw_signal"
-			return 1
-			;;
-	esac
+        RSHIFT)
+            wires[$key]=$((a >> c))
+            ;;
+
+        *)
+            return 1
+            ;;
+        esac
+    done <<<$expression_str
 }
 
 main() {
-	declare -A wires=()
 
-	if [[ "$#" -eq 1 ]]; then
-		parse_signal "$1"
-		return 0
-	fi
-	while IFS= read -r line; do
-		parse_signal "$line"
-		exit 1
-	done
+    while read -r line; do
+        parse_line "${line%%->*}" "${line##*->}"
+    done
+
+    for wire in "${!wires[@]}"; do
+        echo "${wire}=${wires[$wire]}"
+    done
 }
 
 main "$@"
